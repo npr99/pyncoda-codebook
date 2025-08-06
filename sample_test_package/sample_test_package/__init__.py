c4112a0b0fa12ee70f484bdda5ce2063af8be167
Nathanael Rosenheim <nproctor@tamu.edu>
2025-08-06 15:57:35 -0400

    success! made my first package

--- sample_test_package/sample_test_package/__init__.py
+++ sample_test_package/sample_test_package/__init__.py
@@ -7,3 +7,14 @@ def add_num(a: float, b: float) -> float:
     5
     """
     return a + b
+
+
+def subtract_num(a: float, b: float) -> float:
+    """Return the difference of *a* and *b*.
+
+    Examples
+    --------
+    >>> subtract_num(2, 3)
+    -1
+    """
+    return a - b
